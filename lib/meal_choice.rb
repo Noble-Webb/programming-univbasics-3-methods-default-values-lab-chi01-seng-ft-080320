6925755b248762b51be0e2dfc70d94f23fbc7d7f
Justin Webb <webb.n.justin@gmail.com>
2020-07-11 22:46:06 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,6 +1,9 @@
-def meal_choice(veg1, veg2, protien = "tofu")
+def meal_choice(veg1, veg2, protein = "tofu")
   puts "What a nutritious meal!"
+  puts "a plate of #{protein} with #{veg1} and #{veg2}."
+  
 end
+
 
 
 
